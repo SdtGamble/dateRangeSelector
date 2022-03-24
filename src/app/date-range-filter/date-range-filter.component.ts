bd7143076316491ac2016d511cc6ce7919a43824
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 12:55:42 +0000

    Removed event label, used template variables

--- src/app/date-range-filter/date-range-filter.component.ts
+++ src/app/date-range-filter/date-range-filter.component.ts
@@ -14,9 +14,9 @@ export class DateRangeFilterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addEvent(startDate: HTMLInputElement, endDate: HTMLInputElement, event: MatDatepickerInputEvent<Date>) {
+  //Run on date change via calendar selection or enter key
+  dateChanged(startDate: HTMLInputElement, endDate: HTMLInputElement) {
     console.log(startDate.value);
     console.log(endDate.value);
-    console.log(event);
   }
 }
