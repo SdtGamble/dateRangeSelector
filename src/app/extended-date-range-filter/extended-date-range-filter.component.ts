8ac36f14fbf1b5e6d44d07653a4172bc4a38f3c5
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 15:55:22 +0000

    Added base ranges and placeholder text

--- src/app/extended-date-range-filter/extended-date-range-filter.component.ts
+++ src/app/extended-date-range-filter/extended-date-range-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { Moment } from 'moment';
 
 @Component({
@@ -9,6 +10,14 @@ import { Moment } from 'moment';
 export class ExtendedDateRangeFilterComponent implements OnInit {
 
   selected: { startDate: Moment, endDate: Moment };
+  ranges: any = {
+    'Today': [moment(), moment()],
+    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+    'This Month': [moment().startOf('month'), moment().endOf('month')],
+    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+  }
 
   constructor() { }
 
