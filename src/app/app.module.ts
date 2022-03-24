5c731135158d537cb850acd3eea7b4d1e1fbb6b5
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 12:01:57 +0000

    Imported modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DateRangeFilterComponent } from './date-range-filter/date-range-filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule, MatDateRangeInput, MatDateRangePicker } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    DateRangeFilterComponent
+    DateRangeFilterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [DateRangeFilterComponent]
