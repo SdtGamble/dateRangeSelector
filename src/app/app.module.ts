b9505431dd33d4ff8241ba00203256db4de11455
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 11:47:57 +0000

    Removed old files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { DateRangeFilterComponent } from './date-range-filter/date-range-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     DateRangeFilterComponent
   ],
   imports: [
