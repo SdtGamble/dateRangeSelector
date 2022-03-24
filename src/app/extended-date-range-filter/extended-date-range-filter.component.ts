ee531c854681deab45429e607dfcd2994bbf13e0
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 16:17:08 +0000

    Tidying up

--- src/app/extended-date-range-filter/extended-date-range-filter.component.ts
+++ src/app/extended-date-range-filter/extended-date-range-filter.component.ts
@@ -24,10 +24,4 @@ export class ExtendedDateRangeFilterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  //Run on date change via calendar selection or enter key
-  dateChanged(startDate: HTMLInputElement, endDate: HTMLInputElement) {
-    console.log(startDate.value);
-    console.log(endDate.value);
-  }
-
 }
