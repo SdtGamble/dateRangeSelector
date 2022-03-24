353c97fdbcda00264c51b55b3f76d3a9e041a46b
Steven Gamble <Steven.Gamble@digitalspace.co.uk>
2022-03-24 11:45:06 +0000

    Initial commit and bootstrap updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DateRangeFilterComponent } from './date-range-filter/date-range-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateRangeFilterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [DateRangeFilterComponent]
 })
 export class AppModule { }
